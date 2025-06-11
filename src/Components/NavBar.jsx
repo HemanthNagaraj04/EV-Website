f2ab89f89b5710e3e3ad5ccc91c32918d9e5dc54
HemanthNagaraj04 <hemanthnagaraj2004@gmail.com>
2025-06-11 12:02:25 +0530

    made it responsive on mobile devices

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,17 +1,27 @@
-import './NavBar.css'
+import { useState } from 'react';
+import './NavBar.css';
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className='nav'>
       <div className='nav-logo'>EV</div>
-      <ul className='nav-content'>
+
+      <div className='hamburger' onClick={() => setIsOpen(!isOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <ul className={`nav-content ${isOpen ? 'open' : ''}`}>
         <li>Home</li>
         <li>Explore</li>
         <li>About</li>
         <li className='nav-contact'>Contact</li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
